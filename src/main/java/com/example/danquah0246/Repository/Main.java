56432301813baef66fc8db642569ffa09dd690a5
Young-1legend <81308017+Young-1legend@users.noreply.github.com>
2023-07-17 13:39:10 +0000

    debugging...

--- src/main/java/com/example/danquah0246/Repository/Main.java
+++ src/main/java/com/example/danquah0246/Repository/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(Main.class.getClass().getResource("/Main.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Inventory Management");
         primaryStage.setScene(scene);
